eeabdad0a3304d82cd5105356068fd20fccb712e
akshaynarvekar <amn419@nyu.edu>
2017-04-25 17:50:20 -0400

    Handle interactions

--- static/js/view.js
+++ static/js/view.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    /**
+    * Handles resize
+    *
+    */
     var cw = $('.main-options-container').width();
     $('.main-options-container').css({'height':cw+'px'});
     window.onresize = function(event) {
